331829cc750355eec086c1194491d83767a225e9
Marek <markusin969@gmail.com>
2022-12-05 23:39:06 +0100

    ADD values

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 
 int main(int argc ,char *argv[]){
-
+int a;
+size_t x;
     std::cout<<"Udało sie !!!!!"<<std::endl;
     std::cout<<"Jupyy jeee !!!!"<<std::endl;
     std::cout<<"Jupyy jaa !!!!!"<<std::endl;
-     std::cout<<"Jupyy jee !!!!!"<<std::endl;
+    std::cout<<"Jupyy jee !!!!!"<<std::endl;
     return EXIT_SUCCESS;
 }

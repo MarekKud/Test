f6dc691fcd56ac661870c01bf1eb8f3b738def56
Marek <markusin969@gmail.com>
2022-12-05 23:31:26 +0100

    Newline cout

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,6 @@ int main(int argc ,char *argv[]){
     std::cout<<"Udało sie !!!!!"<<std::endl;
     std::cout<<"Jupyy jeee !!!!"<<std::endl;
     std::cout<<"Jupyy jaa !!!!!"<<std::endl;
+     std::cout<<"Jupyy jee !!!!!"<<std::endl;
     return EXIT_SUCCESS;
 }

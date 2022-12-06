03df2bea0e75c751f463335fd78e1b7e7dc5831f
First_Project <markusin969@gmail.com>
2022-12-06 15:22:48 +0100

    delete

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 int main(int argc ,char *argv[]){
     int a;
     size_t x;
-    std::cout<<"Udało sie !!!!!"<<std::endl;
-    std::cout<<"Jupyy jeee !!!!"<<std::endl;
-    std::cout<<"Jupyy jaa !!!!!"<<std::endl;
-    std::cout<<"Jupyy jee !!!!!"<<std::endl;
+    std::cout<<"Success !!!!!"<<std::endl;
+ 
     return EXIT_SUCCESS;
 }
